f7d7dab7b0f231fc6f6722e145f3944fad229b8d
NSCabezon <nscabezon@gmail.com>
2020-04-02 18:41:44 +0200

    initial commit

---
+++ PruebaPagosFX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PruebaPagosFX
+//
+//  Created by Ivan Cabezon on 02/04/2020.
+//  Copyright © 2020 Ivan Cabezon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
